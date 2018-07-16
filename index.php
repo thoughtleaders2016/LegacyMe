3476603046dd4d22e04ebc16f1bd5ca68bf03cf7
thoughtleaders2016 <41284897+thoughtleaders2016@users.noreply.github.com>
2018-07-16 15:05:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+*	ThoughtLeaders  Technologies LLP
+*	14/Jul/2018
+*/
+require_once('access-control.php');
+require_once('db-config.php');
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$data = json_decode( file_get_contents('php://input'),true );
+	$type=$data['type'];
+	require_once('./post-api.php');
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+	require_once('./get-api.php');
+}
---
+++ res/css/style.css
@@ -0,0 +1,4 @@
+/**
+*	ThoughtLeaders  Technologies LLP
+*	14/Jul/2018
+*/
