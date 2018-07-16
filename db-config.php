a21a8fb886b9970b60a4fe4bcb0c1263f4d1a98f
thoughtleaders2016 <41284897+thoughtleaders2016@users.noreply.github.com>
2018-07-16 15:03:19 +0530

    Delete db-config.php

--- db-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_host="localhost";
-$db_user="root";
-$db_password="";
-$db_name="ems";
-$conn=mysqli_connect($db_host,$db_user,$db_password,$db_name);
-if(!$conn) die("Connection Problem !");
